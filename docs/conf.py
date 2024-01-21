c55a828b4a88cfd4e075149944c77d955399c87d
Chris Brody <chris.brody+brodybits@gmail.com>
2024-01-21 16:29:21 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-# XXX TODO: DEFER THIS CHANGE TO PR #2
-# sys.path.insert(0, os.path.abspath("."))
+# NOTE: This code SHOULD NOT BE NEEDED - SEE PR #2
+sys.path.insert(0, os.path.abspath("."))
 
 project = "InquirerLib"
 copyright = "2021, Kevin Zhuang; 2024-present Christopher J. Brody"
